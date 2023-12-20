6507be9d1055000f11b89f940f960298a59fe8e2
Aditya Agarwal <aditya@student.manchester.ac.uk>
2023-12-20 06:13:46 +0000

    Recover frontend files

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#85CED8', // non-photo-blue
+    },
+    secondary: {
+      main: '#7C89A1', // cool-gray
+    },
+  },
+});
+
+export default theme;
