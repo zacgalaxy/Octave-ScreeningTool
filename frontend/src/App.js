458369d91148349e59fcf030eac2630fb14e2846
Aditya Agarwal <aditya@student.manchester.ac.uk>
2023-12-20 07:18:51 +0000

    Implement React Router for frontend navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import { ThemeProvider } from '@emotion/react';
 import theme from './theme';
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <div>
-        front-end placeholder
-      </div>
+      <Router>
+                <Routes>
+                    {/* 
+                    <Route path="/login" exact component={LoginPage} /> // This will be used for the login page
+                    <Route path="/resetPassword/*" exact component={ResetPasswordPage} /> // This will be used to reset the user's password
+                    <Route path="/activate/:verifyToken" component={ActivatePage} />  // This will be used for activating the user's account
+                    */} 
+                    <Route path="/" element={<LandingPage />} />
+                </Routes>
+            </Router>
     </ThemeProvider>
   );
 }
