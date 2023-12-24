21b1bf541c5f33d0d1e75b186722c640a406bb27
v8runn <varunshankar02@gmail.com>
2023-12-24 23:35:39 +0300

    changed routes and implemented screening tool ui

--- frontend/src/components/ProductsContent.js
+++ frontend/src/components/ProductsContent.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Card, CardContent, Typography, ButtonBase, useTheme } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home'; // Example icon
 import AnalyticsIcon from '@mui/icons-material/Analytics'; // Example icon
 import ReportIcon from '@mui/icons-material/Report'; // Example icon
 import SettingsIcon from '@mui/icons-material/Settings'; 
-
+import { useNavigate } from 'react-router-dom';
 
 const ProductsContent = ({ }) => {
     const theme = useTheme();
+    const navigate = useNavigate();
+
     const cardsData = [
-        { title: "Screening", description: "The description here", icon: <HomeIcon  /> },
+        { title: "Screening Tool", description: "The description here", icon: <HomeIcon  /> },
         { title: "Analysis", description: "The description here", icon: <AnalyticsIcon /> },
         { title: "Reporting", description: "The description here", icon: <ReportIcon /> },
         { title: "Management", description: "The description here", icon: <SettingsIcon /> },
     ];
 
-    const handleCardClick = (cardTitle) => {
-        // TODO: My suggestion is to use a ReactHook to change the app-wide state 
-        // where it changes the content being rendered when a user clicks on the screening tool
+    const handleCardClick = (component) => {
+       switch (component) {
+        case "Screening":
+            navigate('/products/screeningtool');
+       }
     };
-
 
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'column' }}>
@@ -30,14 +33,13 @@ const ProductsContent = ({ }) => {
         {cardsData.map(card => (
             <ButtonBase 
                 key={card.title} 
-                onClick={() => handleCardClick(card.title)} 
                 style={{ 
                     margin: theme.spacing(1), 
                     width: `calc(25% - ${theme.spacing(2)})`, 
                     maxWidth: 300, 
                 }}
             >
-                <Card sx={{ width: '100%', borderRadius: theme.shape.borderRadius  }}>
+                <Card sx={{ width: '100%', borderRadius: theme.shape.borderRadius  }} onClick={() => handleCardClick(card.title)} >
                     <CardContent sx={{ backgroundColor: theme.palette.background.paper, color: theme.palette.text.primary }}>
                         {React.cloneElement(card.icon, { sx: { fontSize: '70px' } })}
                         <Typography gutterBottom variant="h5" component="div" sx={{ backgroundColor: theme.palette.primary.dark, color: theme.palette.getContrastText(theme.palette.primary.dark), width: '100%' }}>
---
+++ frontend/src/components/general/HeaderBar.js
@@ -0,0 +1,1 @@
+import AppBar from '@mui/material/AppBar'
